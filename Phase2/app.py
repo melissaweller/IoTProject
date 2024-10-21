2a13139c3fc83cbba3baa75592894a002407dbf8
melissaweller <112568989+melissaweller@users.noreply.github.com>
2024-10-21 13:55:17 -0400

    added email config to send email

--- Phase2/app.py
+++ Phase2/app.py
@@ -1,38 +1,67 @@
-from flask import Flask, render_template, redirect, url_for, jsonify
+import Adafruit_DHT
 import RPi.GPIO as GPIO
+from flask import Flask, jsonify, render_template
+import smtplib
+from email.mime.text import MIMEText
 
 app = Flask(__name__)
 
-LED_PIN = 18
+# GPIO setup for fan
+FAN_PIN = 18
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.setup(FAN_PIN, GPIO.OUT)
 
-led_status = False
+# DHT sensor setup
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 17
+
+# connect with Google's servers
+smtp_ssl_host = 'smtp.gmail.com'
+smtp_ssl_port = 465
+
+# Email configuration
+EMAIL_ADDRESS = "iotproject87@gmail.com"
+EMAIL_PASSWORD = "vucp oftn gupf ttjv" # actual password: iotproject2024
+
+def send_email(temperature):
+    msg = MIMEText(f"The current temperature is {temperature}°C. Would you like to turn on the fan?")
+    msg['Subject'] = 'Temperature Alert'
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = EMAIL_ADDRESS  # Send to yourself for testing
+
+    with smtplib.SMTP(smtp_ssl_host, smtp_ssl_host) as server:
+        server.starttls()
+        server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        server.send_message(msg)
 
 @app.route('/')
 def index():
-    global led_status
-    return render_template('index.html', led_status=led_status)
+    return render_template('index.html')
 
-@app.route('/toggle', methods=['POST'])
-def toggle():
-    global led_status
-    led_status = not led_status
+@app.route('/data')
+def data():
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    
+    if temperature is not None and humidity is not None:
+        if temperature > 24:
+            send_email(temperature)
+        
+        return jsonify({'temperature': temperature, 'humidity': humidity})
+    else:
+        return jsonify({'error': 'Failed to retrieve data'}), 500
 
-    # Toggle the LED
-    GPIO.output(LED_PIN, GPIO.HIGH if led_status else GPIO.LOW)
+@app.route('/fan/<status>')
+def control_fan(status):
+    if status.lower() == 'on':
+        GPIO.output(FAN_PIN, GPIO.HIGH)
+    else:
+        GPIO.output(FAN_PIN, GPIO.LOW)
+    return '', 204
 
-    return '', 204  
-
-@app.route('/status')
-def status():
-    global led_status
-    return jsonify({'led_status': led_status})
-
-if __name__ == '__main__': 
+if __name__ == '__main__':
     try:
         app.run(host='0.0.0.0', port=5000, debug=True)
     except KeyboardInterrupt:
         pass
     finally:
-        GPIO.cleanup()  # Ensure GPIO cleanup when the app is stopped
+        GPIO.cleanup()  # Cleanup GPIO on exit
